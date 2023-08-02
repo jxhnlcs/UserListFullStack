bc5092078dcd5038d58873ee790125e8bbd0e2b6
jxhnlcs <joohnzay@gmail.com>
2023-08-01 22:58:02 -0300

    update

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,6 +2,22 @@ import express from 'express'
 
 const app = express()
 
+app.get('/users', (request, response) => {
+  return response.json(['Usuário 1', 'Usuário 2'])
+})
+
+app.post('/users', (request, response) => {
+  return response.json({ message: 'Criando usuário' })
+})
+
+app.put('/users', (request, response) => {
+  return response.json({ message: 'Atualizando usuário' })
+})
+
+app.delete('/users', (request, response) => {
+  return response.json({ message: 'Excluindo usuário' })
+})
+
 app.listen('3333', () => {
   console.log('API rodando com sucesso na porta 3333!')
 })
