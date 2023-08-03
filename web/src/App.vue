463e81f95c2186cab5f5eb996decb6e67700b440
John Lucas <joohnzay@gmail.com>
2023-08-03 12:23:54 -0300

    componentização

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <h1>Hello World</h1>
+  <Header/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Header from '@/components/Header.vue'
 
 export default defineComponent({
   data() {
     return {
       name: 'Cataline',
     }
+  },
+
+  components: {
+    Header,
   },
 })
 </script>
---
+++ web/src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <h1>Logo</h1>
+    <ul>
+      <li>Home</li>
+      <li>Contato</li>
+    </ul>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({})
+</script>
